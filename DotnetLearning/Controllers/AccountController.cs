4b5d817ffb9cd84655c989021e6edcbb07e49cd4
sai venkat <sai.rajam@keyloop.com>
2026-03-10 21:57:24 +0530

    update retry settigns for DB

--- DotnetLearning/Controllers/AccountController.cs
+++ DotnetLearning/Controllers/AccountController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.WebUtilities;
 using System.Security.Claims;
 
 namespace DotnetLearning.Controllers
